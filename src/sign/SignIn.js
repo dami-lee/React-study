ba3779565877874c74eaa642226ee49d3c76a19b
dami-lee <dami223@daum.net>
2019-11-10 22:56:52 +0900

    onChange event bind email, password

--- src/sign/SignIn.js
+++ src/sign/SignIn.js
@@ -1,10 +1,51 @@
 import React from 'react';
+import Input from '../Components/Input';
+import Button from '../Components/Button';
 
 class SignIn extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
+
+    updateEmail = event => {
+        this.setState({
+            email: event.target.value
+        })
+    }
+
+    updatePassword = event => {
+        this.setState({
+            password: event.target.value
+        })
+    }
+
     render() {
+        const { email, password } = this.state;
+        const {
+            updateEmail,
+            updatePassword
+        } = this;
+
         return (
             <>
                 <h2>Sign In</h2>
+                <Input
+                    name="Email"
+                    value={email}
+                    onChange={updateEmail}/>
+                <Input
+                    type="password"
+                    name="Password"
+                    value={password}
+                    onChange={updatePassword}/>
+                <Button>
+                    Submit
+                </Button>
             </>
         );
     }
