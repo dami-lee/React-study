65241fa91361af97c1aebfc4a87ab9c5205ae0fa
dami-lee <dami223@daum.net>
2019-11-10 23:12:18 +0900

    onChange event bind first_name, email, password

--- src/sign/SignUp.js
+++ src/sign/SignUp.js
@@ -1,10 +1,59 @@
 import React from 'react';
+import Input from '../Components/Input';
 
 class SignUp extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            first_name: '',
+            email: '',
+            password: ''
+        }
+    }
+
+    updateFirstName = event => {
+        this.setState({
+            first_name: event.target.value
+        })
+    }
+
+    updateEmail = event => {
+        this.setState({
+            email: event.target.value
+        })
+    }
+
+    updatePassword = event => {
+        this.setState({
+            password: event.target.value
+        })
+    }
+
     render() {
+        const { first_name, email, password } = this.state;
+        const {
+            updateFirstName,
+            updateEmail,
+            updatePassword
+        } = this;
+
         return (
             <>
                 <h2>Sign Up</h2>
+                <Input
+                    name="FirstName"
+                    value={first_name}
+                    onChange={updateFirstName}/>
+                <Input
+                    name="Email"
+                    value={email}
+                    onChange={updateEmail}/>
+                <Input
+                    type="password"
+                    name="Password"
+                    value={password}
+                    onChange={updatePassword}/>
             </>
         );
     }
