1afc112b008ad0630e8c0d664bcc66d2490591e7
dami-lee <dami223@daum.net>
2019-11-10 22:13:29 +0900

    Declared click event

--- src/sign/Sign.js
+++ src/sign/Sign.js
@@ -4,15 +4,37 @@ import SignIn from './SignIn';
 import SignUp from './SignUp';
 
 class Sign extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            show: false
+        }
+    }
+
+    showSignIn = () => {
+        this.setState({
+            show: 'SignIn'
+        })
+    }
+
+    showSignUp = () => {
+        this.setState({
+            show: 'SignUp'
+        })
+    }
+
     render() {
+        const { show } = this.state;
+
         return (
             <>
                 <h1>Sign</h1>
-                <Button>Sign In</Button>
-                <Button>Sign Up</Button>
-                
-                <SignIn/>
-                <SignUp/>
+                <Button onClick={this.showSignIn}>Sign In</Button>
+                <Button onClick={this.showSignUp}>Sign Up</Button>
+
+                { show === 'SignIn' && <SignIn/> }
+                { show === 'SignUp' && <SignUp/> }
             </>
         );
     }
