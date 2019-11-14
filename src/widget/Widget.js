83c26ae90e5fd0917f8c2e2ddaf47428f773ac46
dami-lee <dami223@daum.net>
2019-11-14 09:53:09 +0900

    Unmount test

--- src/widget/Widget.js
+++ src/widget/Widget.js
@@ -2,9 +2,34 @@ import React from 'react';
 import Timer from './Timer';
 
 class Widget extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            show: 'time' // intro, time
+        }
+    }
+
+    toggleShow = () => {
+        if ( this.state.show === 'time' ) {
+            this.setState({
+                show: 'intro'
+            })
+        } else {
+            this.setState({
+                show: 'time'
+            })
+        }
+    }
+
     render() {
         return (
-            <Timer/>
+            <>
+                {
+                    this.state.show === 'time' && <Timer/>
+                }
+                <button onClick={this.toggleShow}>changed show state</button>
+            </>
         );
     }
 }
